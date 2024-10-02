497de19f971b743abde8c301362036d1be03dae4
rasel mridha <raselmridha792@gmail.com>
2024-10-03 01:06:56 +0600

    implementing rest api

---
+++ js/test.js
@@ -0,0 +1,7 @@
+function getTimeString(time){
+    const hour = parseInt(time/3600);
+    let reminingSecond = time%3600;
+    const minute = parseInt(reminingSecond/60);
+    reminingSecond = (reminingSecond%60);
+    return `${hour}hour ${minute} minute ${reminingSecond}second`;
+}
