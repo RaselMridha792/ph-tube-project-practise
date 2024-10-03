6c41aceb57a788f84e1ef4c899e23aad5cf8b20d
rasel mridha <raselmridha792@gmail.com>
2024-10-03 15:24:13 +0600

    add load video function when button clicked and only category wise video are showing on these section

--- js/video.js
+++ js/video.js
@@ -15,6 +15,14 @@ function getTimeString(time){
     return `${hour}hour ${minute} minute ${reminingSecond}second`;
 }
 
+// load categories videos 
+const loadCategoryVideos = (id) =>{
+    // alert(id);
+    fetch(`https://openapi.programming-hero.com/api/phero-tube/category/${id}`)
+        .then(res => res.json())
+        .then(data =>displayVideos(data.category))
+        .catch(error => console.log(error));
+}
 
 // display categories 
 const displayCategories = (categories) => {
@@ -22,12 +30,15 @@ const displayCategories = (categories) => {
     categories.forEach((item) => {
         console.log(item);
         // create a button
-        const button = document.createElement('button');
-        button.classList = "btn";
-        button.innerText = item.category;
+        const buttonContainer = document.createElement("div");
+        buttonContainer.innerHTML = `
+            <button onclick="loadCategoryVideos(${item.category_id})" class="btn">
+                ${item.category}
+            </button>
+        `
 
         // add button category to container 
-        categoriesContainer.appendChild(button);
+        categoriesContainer.appendChild(buttonContainer);
 
     })
 }
@@ -42,27 +53,44 @@ const loadVideos = () => {
         .catch(error => console.log(error))
 }
 
-const demoCard = {
-    "category_id": "1001",
-    "video_id": "aaaa",
-    "thumbnail": "https://i.ibb.co/L1b6xSq/shape.jpg",
-    "title": "Shape of You",
-    "authors": [
-        {
-            "profile_picture": "https://i.ibb.co/D9wWRM6/olivia.jpg",
-            "profile_name": "Olivia Mitchell",
-            "verified": ""
-        }
-    ],
-    "others": {
-        "views": "100K",
-        "posted_date": "16278"
-    },
-    "description": "Dive into the rhythm of 'Shape of You,' a captivating track that blends pop sensibilities with vibrant beats. Created by Olivia Mitchell, this song has already gained 100K views since its release. With its infectious melody and heartfelt lyrics, 'Shape of You' is perfect for fans looking for an uplifting musical experience. Let the music take over as Olivia's vocal prowess and unique style create a memorable listening journey."
-}
+// const demoCard = {
+//     "category_id": "1001",
+//     "video_id": "aaaa",
+//     "thumbnail": "https://i.ibb.co/L1b6xSq/shape.jpg",
+//     "title": "Shape of You",
+//     "authors": [
+//         {
+//             "profile_picture": "https://i.ibb.co/D9wWRM6/olivia.jpg",
+//             "profile_name": "Olivia Mitchell",
+//             "verified": ""
+//         }
+//     ],
+//     "others": {
+//         "views": "100K",
+//         "posted_date": "16278"
+//     },
+//     "description": "Dive into the rhythm of 'Shape of You,' a captivating track that blends pop sensibilities with vibrant beats. Created by Olivia Mitchell, this song has already gained 100K views since its release. With its infectious melody and heartfelt lyrics, 'Shape of You' is perfect for fans looking for an uplifting musical experience. Let the music take over as Olivia's vocal prowess and unique style create a memorable listening journey."
+// }
 loadVideos()
 const displayVideos = (videos) => {
-    const videoContainer = document.getElementById('videos')
+    const videoContainer = document.getElementById('videos');
+    videoContainer.innerHTML ="";
+
+    if(videos.length ==0){
+        videoContainer.classList.remove('grid');
+        videoContainer.innerHTML =`
+            <div class="min-h-[300px] w-full flex flex-col gap-5 justify-center items-center">
+                <img src="./images/Icon.png" />
+            </div>
+            <h2 class="font-bold text-center text-xl "> No Content here In This Category
+            </h2>
+        `;
+        return;
+    }
+    else{
+        videoContainer.classList.add("grid");
+    }
+
     videos.forEach(video => {
         console.log(video);
         const card = document.createElement('div');
